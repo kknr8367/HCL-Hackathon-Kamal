8b832e6e97202f6301a6f31c0c9369f3583198fd
Kallutla Kamalanadha Reddy <kamalanadha.r@simplify3x.com>
2025-05-30 16:39:00 +0530

    pipeline modification

--- modules/main.tf
+++ modules/main.tf
@@ -1,22 +1,22 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./vpc.tf"  # ✅ Directly referencing the Terraform file
 }
 
 module "security_group" {
-  source = "./modules/sg"
+  source = "./sg.tf"  # ✅ Directly referencing the Terraform file
   vpc_id = module.vpc.vpc_id
 }
 
 module "iam" {
-  source = "./modules/iam"
+  source = "./iam.tf"  # ✅ Directly referencing the Terraform file
 }
 
 module "ecr" {
-  source = "./modules/ecr"
+  source = "./ecr.tf"  # ✅ Directly referencing the Terraform file
 }
 
 module "ecs_fargate" {
-  source         = "./modules/ecs_fargate"
+  source         = "./ecs_fargate.tf"  # ✅ Directly referencing the Terraform file
   vpc_id         = module.vpc.vpc_id
   security_group = module.security_group.sg_id
   iam_role       = module.iam.ecs_task_execution_role_arn
@@ -24,6 +24,6 @@ module "ecs_fargate" {
 }
 
 module "cloudwatch" {
-  source = "./modules/cloudwatch"
+  source = "./cloudwatch.tf"  # ✅ Directly referencing the Terraform file
   ecs_cluster_name = module.ecs_fargate.cluster_name
 }
